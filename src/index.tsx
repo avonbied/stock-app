0468917ea6296e8818eea812c63823073bc63494
Lex von Biedenfeld <58815784+avonbied@users.noreply.github.com>
2021-05-06 02:40:45 -0500

    Added app entry files

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (<h1>React + TypeScript! {new Date().toLocaleDateString()}</h1>);
+  
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
