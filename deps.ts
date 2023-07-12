2fec439ac17ad359ae979265a79abb82ac49306a
Lex <58815784+avonbied@users.noreply.github.com>
2023-07-11 21:40:56 -0500

    Update dashboard service

--- deps.ts
+++
@@ -1,4 +0,0 @@
-export { opine } from "https://deno.land/x/opine/mod.ts";
-
-export {h, hydrate as preactHydrate} from 'https://cdn.skypack.dev/preact';
-export * as preactRenderToString from 'https://cdn.skypack.dev/preact-render-to-string';
